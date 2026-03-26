7ed4d9381db157f2a5fdd1646e42e7f15471c652
Galvin Radley Ngo <51685547+galvinradleyngo@users.noreply.github.com>
2026-03-26 04:11:30 +0000

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cognitive-makeover/', // <-- ADD THIS LINE (must match your exact repo name)
 })
